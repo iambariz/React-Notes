816cc589892c20817322ed4236031c9b8a84f94a
iambariz <61651210+iambariz@users.noreply.github.com>
2021-07-16 11:33:11 +0100

    Sidebar extension

--- main/src/components/Sidebar/SideBar.js
+++ main/src/components/Sidebar/SideBar.js
@@ -15,7 +15,15 @@ const SideBar = (props) => {
     <div className="sidebar">
         <h1>  <span className="username">John</span>'s notes</h1>
         <div className="div"><a href="#" className="btn btn-create">New note <i class="fas fa-plus-circle"></i></a> </div>
-        <div className="div"></div>
+        <div className="div">
+            <h2>My notes:</h2>
+            <ul className="note-ul">
+                <li className="note-li">Test 1  <i className="fas fa-times btn-sidebar" ></i></li>
+                <li className="note-li">Test 2  <i className="fas fa-times btn-sidebar" ></i></li>
+                <li className="note-li">Test 3  <i className="fas fa-times btn-sidebar" ></i></li>
+                <li className="note-li">Test 4  <i className="fas fa-times btn-sidebar" ></i></li>
+            </ul>
+        </div>
         <div className="div"></div>
     </div>)
 }
